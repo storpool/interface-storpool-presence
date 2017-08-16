8d45f9190b608a1029ad3ac358e6014483838fa4
Peter Pentchev <pp@storpool.com>
2017-08-16 09:24:35 -0400

    Turn this into the storpool-presence interface.

--- provides.py
+++ provides.py
@@ -1,30 +1,18 @@
 import json
-import platform
 import time
 
-from charmhelpers.core import hookenv
 from charms import reactive
+
+from charmhelpers.core import hookenv
 
 def rdebug(s):
 	with open('/tmp/storpool-charms.log', 'a') as f:
-		print('{tm} [storpool-config-provides] {s}'.format(tm=time.ctime(), s=s), file=f)
+		print('{tm} [storpool-presence-provides] {s}'.format(tm=time.ctime(), s=s), file=f)
 
-class StorPoolConfigProvides(reactive.RelationBase):
-	scope = reactive.scopes.UNIT
-	sp_node = platform.node()
+class StorPoolPresenceProvides(reactive.RelationBase):
+	scope = reactive.scopes.GLOBAL
 
-	@reactive.hook('{provides:storpool-config}-relation-{joined,changed}')
+	@reactive.hook('{provides:storpool-presence}-relation-{joined,changed}')
 	def changed(self):
-		self.set_state('{relation_name}.available')
-
-	@reactive.hook('{provides:storpool-config}-relation-departed')
-	def departed(self):
-		self.remove_state('{relation_name}.available')
-	
-	def configure(self, nodedata, extra_hostname=None):
-		rdebug('config-provides-configure invoked with {ks} keys'.format(ks=len(nodedata.keys())))
-		data = { self.sp_node: nodedata, }
-		if extra_hostname is not None:
-			data[extra_hostname] = nodedata
-		rdebug('config-provides-configure sending some data out, keys: {ks}'.format(ks=sorted(data.keys())))
-		self.set_remote('storpool-config', json.dumps(data))
+		rdebug('relation-joined/changed, setting the notify state to kick something off')
+		self.set_state('{relation_name}.notify')
--- requires.py
+++ requires.py
@@ -8,21 +8,20 @@ from charms import reactive
 
 def rdebug(s):
 	with open('/tmp/storpool-charms.log', 'a') as f:
-		print('{tm} [storpool-config-requires] {s}'.format(tm=time.ctime(), s=s), file=f)
+		print('{tm} [storpool-presence-requires] {s}'.format(tm=time.ctime(), s=s), file=f)
 
-class StorPoolRepoRequires(reactive.RelationBase):
-	scope = reactive.scopes.UNIT
+class StorPoolPresenceRequires(reactive.RelationBase):
+	scope = reactive.scopes.GLOBAL
 	sp_node = platform.node()
 
-	@reactive.hook('{requires:storpool-config}-relation-{joined,changed}')
+	@reactive.hook('{requires:storpool-presence}-relation-{joined,changed}')
 	def changed(self):
 		rdebug('relation-joined/changed invoked')
 		conv = self.conversation()
-		spconf = conv.get_remote('storpool-config')
+		spconf = conv.get_remote('storpool_presence')
 		if spconf is None:
-			rdebug('no config yet')
-			conv.set_local('storpool-config', None)
-			self.remove_state('{relation_name}.available')
+			rdebug('no presence data yet')
+			conv.set_local('storpool_presence', None)
 		else:
 			rdebug('whee, we got something from the {key} conversation, trying to deserialize it'.format(key=conv.key))
 			try:
@@ -30,40 +29,12 @@ class StorPoolRepoRequires(reactive.RelationBase):
 				rdebug('got something: type {t}, dict keys: {k}'.format(t=type(conf).__name__, k=sorted(conf.keys()) if isinstance(conf, dict) else []))
 				if not isinstance(conf, dict):
 					rdebug('well, it is not a dictionary, is it?')
-					conv.set_local('storpool-config', None)
+					conv.set_local('storpool_presence', None)
 					return
-				conv.set_local('storpool-config', conf)
-				if self.sp_node in conf:
-					rdebug('we have some configuration for our node!')
-					self.set_state('{relation_name}.available')
+				conv.set_local('storpool_presence', conf)
+				if conf.get(self.sp_node, False):
+					rdebug('our node seems to be configured!')
+					self.set_state('{relation_name}.configure')
 			except Exception as e:
-				rdebug('oof, could not parse the config passed down the hook: {e}'.format(e=e))
-				conv.set_local('storpool-config', None)
-
-	@reactive.hook('{requires:storpool-config}-relation-departed')
-	def departed(self):
-		conv = self.conversation()
-		c = conv.get_local('storpool-config')
-		if c is not None and isinstance(c, dict):
-			ks = sorted(c.keys())
-			rdebug('oof, departed invoked for {ks}'.format(ks=ks))
-			if self.sp_node in ks:
-				rdebug('...and it was on our node, no less')
-				self.remove_state('{relation_name}.available')
-		conv.set_local('storpool-config', None)
-	
-	def get_storpool_config(self):
-		cfg = None
-		for conv in self.conversations():
-			ncfg = conv.get_local('storpool-config')
-			if ncfg is not None:
-				nkeys = list(sorted(ncfg.keys()))
-				rdebug('get_storpool_config(): {key}: {ks}'.format(key=conv.key, ks=nkeys))
-				if cfg is not None:
-					for key in nkeys:
-						cfg[key] = ncfg[key]
-				else:
-					cfg = ncfg
-		if cfg is not None:
-			rdebug('get_storpool_config() returning keys: {ks}'.format(ks=sorted(cfg.keys())))
-		return cfg
+				rdebug('oof, could not parse the presence data passed down the hook: {e}'.format(e=e))
+				conv.set_local('storpool_presence', None)
